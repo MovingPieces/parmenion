e6e9914b546f48c45d9ab3aa1765d659bcd4eeb0
Jacek Królikowski <nietaki@gmail.com>
2022-08-07 00:30:53 +0200

    add p5.js to the project

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+  let myCanvas = createCanvas(800, 600);
+  myCanvas.parent('canvasContainer');
+}
+
+function draw() {
+  background(0, 100, 0);
+  // put drawing code here
+}
