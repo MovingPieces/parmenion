637dce661cf5ed64e658224d4395265e812c03ff
Jacek Królikowski <nietaki@gmail.com>
2022-08-07 02:51:05 +0200

    start of the interface for the coords

---
+++ js/logic/coords.js
@@ -0,0 +1,47 @@
+class Coords {
+  theta;
+  rho;
+
+  constructor(theta, rho) {
+    this.theta = theta;
+    this.rho = rho;
+  }
+
+  copy() {
+    return new Coords(this.theta, this.rho);
+  }
+
+  simplify() {
+    //TODO
+  }
+
+  x() {
+    //TODO
+  }
+
+  y() {
+    //TODO
+  }
+
+  phi() {
+    //TODO
+  }
+
+  r() {
+    return this.rho;
+  }
+
+  static FromXY(x, y){
+    // TODO
+    return new Coords(0, 0)
+  }
+
+  static FromPhiR(phi, r){
+    // TODO
+    return new Coords(0, 0)
+  }
+
+  static Distance(a, b) {
+    // TODO assume both are coords and return the line distance
+  }
+}
